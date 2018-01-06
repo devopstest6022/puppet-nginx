64a424addd215ed7ca7be7e31f4e4a4ea3b2727f
root <root@shashiudawa1.mylabserver.com>
2018-01-06 19:30:20 +0000

    try

--- manifests/config.pp
+++ manifests/config.pp
@@ -32,4 +32,12 @@ class nginx::config(
    group   => $group,
    mode    => '0775',
  }
+ 
+ file { $config_confd:
+   ensure           => directory,
+      recurse       => true,
+         owner      => $owner,
+            group   => $group,
+               mode => '0775',
+ }
 }
